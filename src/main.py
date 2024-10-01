e2c807bac123f6a1ef5599652822c1457fcc8a66
DenisioMytnysiano <den.mytnyk@ua.fm>
2024-10-01 22:08:44 +0300

    Added category initialization

--- src/main.py
+++ src/main.py
@@ -2,17 +2,33 @@ from contextlib import asynccontextmanager
 import uvicorn
 from dotenv import load_dotenv
 from api.v1.api import api_router
+from api.v1.deps import category_service
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from domain.features.category.exceptions import DuplicateCategoryException
 from infrastructure.db.weaviate.client import client as weaviate
 from infrastructure.db.mongo.database import client as mongo
 
 load_dotenv()
 
+def initialize_categories():
+    predefined_categories = [
+        "Visa and Passport Services",
+        "Diplomatic Inquiries",
+        "Travel Advisories",
+        "Consular Assistance",
+        "Trade and Economic Cooperation"
+    ]
+    try:
+        for category in predefined_categories:
+            category_service.add_category(category, [])
+    except DuplicateCategoryException:
+        pass
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    
+    initialize_categories()
     yield
     weaviate.close()
     mongo.close()
